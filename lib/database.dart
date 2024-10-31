2c8d7a02f301ffa1e495f5d001a0cb71931c780a
meera <meerapm2020@gmail.com>
2024-10-31 08:41:50 +0530

    first commit

---
+++ lib/database.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Database {
+  static List<Map<String, dynamic>> myList = [
+    {
+      "name": "Burger",
+      "color": Colors.red,
+      "Textcolor": Colors.white,
+    },
+    {
+      "name": "Pizza",
+      "color": Colors.white,
+      "Textcolor": Colors.black,
+    },
+    {
+      "name": "Soups",
+      "color": Colors.white,
+      "Textcolor": Colors.black,
+    },
+    {
+      "name": "Soups",
+      "color": Colors.white,
+      "Textcolor": Colors.black,
+    },
+    {
+      "name": "Soups",
+      "color": Colors.white,
+      "Textcolor": Colors.black,
+    },
+  ];
+  static List<Map<String, dynamic>> mynewList = [
+    {
+      "image": "assets/images/burgerp1.jpg",
+      "name": "Double Burger",
+      "subtitle": "Double Beef",
+      "price": "7.50",
+    },
+    {
+      "image": "assets/images/burgerp2.jpg",
+      "name": "Beef Burger",
+      "subtitle": "Cheesy Mozarella",
+      "price": "7.50",
+    },
+    {
+      "image": "assets/images/burgerp3.jpg",
+      "name": "Double Burger",
+      "subtitle": "Double Beef",
+      "price": "7.50",
+    },
+    {
+      "image": "assets/images/burgerp4.jpeg",
+      "name": "Pizza",
+      "subtitle": "Double Beef",
+      "price": "7.50",
+    },
+    {
+      "image": "assets/images/burgerp2.jpg",
+      "name": "Beef Burger",
+      "subtitle": "Cheesy Mozarella",
+      "price": "7.50",
+    },
+    {
+      "image": "assets/images/burgerp3.jpg",
+      "name": "Double Burger",
+      "subtitle": "Double Beef",
+      "price": "7.50",
+    },
+  ];
+
+  static List<Map<String, dynamic>> itemsList = [
+    {
+      "image": "assets/images/cheese.jpg",
+      "color": Colors.purple[200],
+    },
+    {
+      "image": "assets/images/beefsteak.jpg",
+      "color": Colors.lightBlue[200],
+    },
+    {
+      "image": "assets/images/onion.jpg",
+      "color": Colors.orange[100],
+    },
+    {
+      "image": "assets/images/tomato.jpg",
+      "color": Colors.pink[100],
+    },
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:burger_project/details_screen.dart';
+import 'package:burger_project/first_screen.dart';
+import 'package:burger_project/home_screen.dart';
+import 'package:burger_project/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('myBox');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
+  }
+}
